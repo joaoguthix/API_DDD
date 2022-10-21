b4d35a5ebdb0097d2733a8e3c64c3a0e4612aaff
joao vitor <guerreiroxx8@gmail.com>
2022-10-21 16:50:37 -0300

    projeto DDD

---
+++ API_DDD_2022/Domain/Interfaces/Generics/IGenerics.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Interfaces.Generics
+{
+    public interface IGenerics<T> where T : class
+    {
+
+        Task Add(T objeto);
+        Task Update(T objeto);
+        Task Delete(T objeto);
+        Task<T> GetEntityById(int Id);
+        Task<List<T>> List();
+    }
+}
---
+++ API_DDD_2022/Domain/Services/ServiceMessage.cs
@@ -0,0 +1,20 @@
+﻿using Domain.Interfaces;
+using Domain.Interfaces.InterfaceService;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Services
+{
+    public class ServiceMessage : IServiceMessage
+    {
+        private readonly IMessage _Imessage;
+
+        public ServiceMessage(IMessage Imessage)
+        {
+            _Imessage = Imessage;
+        }
+    }
+}
